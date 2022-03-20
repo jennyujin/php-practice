0dbe9678d5da505144748d6fc3505c189c64d68f
jennyujin <eppunijyj@gmail.com>
2022-03-20 22:56:30 +0900

    defense the Cross site scripting

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ $sql = "SELECT * FROM topic";
 $result = mysqli_query($conn, $sql);
 $list = '';
 while($row = mysqli_fetch_array($result)) {
-  $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  $escaped_title = htmlspecialchars($row['title']);
+  $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$escaped_title}</a></li>";
 }
 $article = array(
   'title'=>'Welcome',
@@ -19,8 +20,8 @@ if(isset($_GET['id'])) {
   $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
   $result = mysqli_query($conn, $sql);
   $row = mysqli_fetch_array($result);
-  $article['title'] = $row['title'];
-  $article['description'] = $row['description'];
+  $article['title'] = htmlspecialchars($row['title']);
+  $article['description'] = htmlspecialchars($row['description']);
 }
 ?>
 <!doctype html>
